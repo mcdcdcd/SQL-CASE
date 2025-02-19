14c3be402d0faa3dd361da1867392cfdd3525083
mcdcdcd <87680973+mcdcdcd@users.noreply.github.com>
2025-02-19 11:56:40 -0500

    high-value customers.sql
                                                             --Problem Statement
    --As a data analyst, one of the key tasks is to identify high-value customers based on their spending habits.
    --The goal of this SQL query is to find customers whose total invoice amounts exceed the average total invoice amount across all customers. 
    --This insight can help businesses target their most valuable customers for loyalty programs, special offers, or personalized marketing campaigns.
                                                             
                                                    
    -- We are working with a dataset that contains two main tables:
    --1.Customer - Contains customer details such as CustomerId, FirstName, and City.
    --2.Invoice - Contains invoice details including InvoiceId, CustomerId, and Total of each invoice.

--- Scriptavg&sumsql.sql
+++ Scriptavg&sumsql.sql
@@ -1,14 +1,4 @@
-                                                         --Problem Statement
---As a data analyst, one of the key tasks is to identify high-value customers based on their spending habits.
---The goal of this SQL query is to find customers whose total invoice amounts exceed the average total invoice amount across all customers. 
---This insight can help businesses target their most valuable customers for loyalty programs, special offers, or personalized marketing campaigns.
-                                                         
-                                                
--- We are working with a dataset that contains two main tables:
---1.Customer - Contains customer details such as CustomerId, FirstName, and City.
---2.Invoice - Contains invoice details including InvoiceId, CustomerId, and Total of each invoice.
-
-                                                         
+                                           
 -- Solution
                                                                                                              
 WITH sumtotal AS (    
