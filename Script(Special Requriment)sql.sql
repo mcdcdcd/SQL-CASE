3180a332d9c7a4d5a025f15a15e7454f896fbe6b
mcdcdcd <87680973+mcdcdcd@users.noreply.github.com>
2025-02-18 19:50:20 -0500

    Script(Special Requriment)sql.sql
    --It is really important when we need to add a discount, take into account the specifications.  
    --If the customer has been buying more than 40, they're going to get the 50 off discount. 
    --to receive the discount, and in addition if they live in the states we will add 30 off

--- Script(Special Requriment)sql.sql
+++ Script(Special Requriment)sql.sql
@@ -4,13 +4,13 @@
 SELECT 
     c.CustomerId AS Id_Customer, 
     c.FirstName, 
-    COUNT(i.InvoiceId) AS Total_Invoices, 
-    SUM(i.Total) AS total_bill,
+    COUNT(i.InvoiceId) AS Total_Invoices, --Count Customer´s bills 
+    SUM(i.Total) AS total_bill, --Sum total_bill
     c.Country,
     
     CASE 
-        WHEN SUM(i.Total) > 40 AND c.Country = 'USA' THEN '70% OFF'
-        WHEN SUM(i.Total) > 40 THEN '50% OFF'
+        WHEN SUM(i.Total) > 40 AND c.Country = 'USA' THEN '70% OFF'  -- If the customer  is living in USA add 70 off
+        WHEN SUM(i.Total) > 40 THEN '50% OFF'    -- When the total amount of each customer exceeds $40 so add 50 off 
         ELSE 'Requerimiento no elegible'
     END AS Discount
     
@@ -18,7 +18,5 @@ FROM Customer c
 INNER JOIN Invoice i ON c.CustomerId = i.CustomerId 
 GROUP BY c.CustomerId, c.FirstName, c.Country 
 ORDER BY total_bill DESC;
-
-
 
 
